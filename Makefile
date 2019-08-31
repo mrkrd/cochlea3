66b28e37476938e9b15069299395d2fb0e7fd7ba
mr <>
2019-08-31 23:16:29 +0200

    wip: cochlea → cochlea3 for Python 3 !!!

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	python setup.py build_ext --inplace
+	python3 setup.py build_ext --inplace
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
@@ -6,62 +6,57 @@ from Cython.Build import cythonize
 import numpy
 
 
-
 with open('README.rst') as file:
     long_description = file.read()
 
 
-
-
 extensions = [
     Extension(
-        "cochlea.zilany2009._pycat",
+        "cochlea3.zilany2009._pycat",
         [
-            "cochlea/zilany2009/_pycat.pyx",
-            "cochlea/zilany2009/catmodel.c",
-            "cochlea/zilany2009/complex.c"
+            "cochlea3/zilany2009/_pycat.pyx",
+            "cochlea3/zilany2009/catmodel.c",
+            "cochlea3/zilany2009/complex.c"
         ]
     ),
     Extension(
-        "cochlea.holmberg2007._traveling_waves",
+        "cochlea3.holmberg2007._traveling_waves",
         [
-            "cochlea/holmberg2007/_traveling_waves.pyx",
+            "cochlea3/holmberg2007/_traveling_waves.pyx",
         ]
     ),
     Extension(
-        "cochlea.zilany2014._zilany2014",
+        "cochlea3.zilany2014._zilany2014",
         [
-            "cochlea/zilany2014/_zilany2014.pyx",
-            "cochlea/zilany2014/model_IHC.c",
-            "cochlea/zilany2014/model_Synapse.c",
-            "cochlea/zilany2014/complex.c"
+            "cochlea3/zilany2014/_zilany2014.pyx",
+            "cochlea3/zilany2014/model_IHC.c",
+            "cochlea3/zilany2014/model_Synapse.c",
+            "cochlea3/zilany2014/complex.c"
         ]
     ),
 ]
 
 
-
 setup(
-    name = "cochlea",
-    version = "2",
-    author = "Marek Rudnicki",
-    author_email = "marek.rudnicki@tum.de",
+    name="cochlea3",
+    version="1",
+    author="Marek Rudnicki",
+    author_email="marekrud@posteo.de",
 
-    description = "Inner ear models in Python",
-    license = "GPLv3+",
-    url = "https://github.com/mrkrd/cochlea",
-    download_url = "https://github.com/mrkrd/cochlea/tarball/master",
+    description="Inner ear models for Python 3",
+    license="GPLv3+",
+    url="https://github.com/mrkrd/cochlea3",
+    download_url="https://github.com/mrkrd/cochlea3/tarball/master",
 
-    packages = find_packages(),
-    scripts = ["scripts/run_zilany2014"],
-    package_data = {
-        "cochlea.asr": ["*.csv"]
+    packages=find_packages(),
+    package_data={
+        "cochlea3.asr": ["*.csv"]
     },
-    include_dirs = [numpy.get_include()],
-    ext_modules = cythonize(extensions),
-    long_description = long_description,
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
+    include_dirs=[numpy.get_include()],
+    ext_modules=cythonize(extensions),
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
         "Environment :: Console",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
@@ -69,11 +64,11 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Cython",
         "Programming Language :: C",
     ],
 
-    platforms = ["Linux", "Windows", "FreeBSD", "OSX"],
-    install_requires=["numpy", "pandas", "scipy"],
+    platforms=["Linux", "Windows", "FreeBSD", "OSX"],
+    install_requires=["numpy", "scipy"],
 )
