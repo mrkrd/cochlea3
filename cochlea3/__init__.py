8aa1455cb769f08ad8821dc66e9a51596f336283
mr <>
2019-10-10 22:22:36 +0200

    init: cleanup

--- cochlea3/__init__.py
+++ cochlea3/__init__.py
@@ -1,53 +1,22 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright 2009-2019 Marek Rudnicki
-#
-# This file is part of cochlea.
-#
-# cochlea is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# cochlea is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with cochlea.  If not, see <http://www.gnu.org/licenses/>.
 
 """Innear ear models in Python.
 
 """
 
-from __future__ import division, print_function, absolute_import
-
-
-import warnings
-import sys
 import numpy as np
 
-from cochlea.zilany2009 import run_zilany2009
-from cochlea.holmberg2007 import run_holmberg2007
-from cochlea.holmberg2007 import run_holmberg2007_vesicles
-from cochlea.zilany2014 import run_zilany2014
-from cochlea.zilany2014 import run_zilany2014_rate
+from .zilany2009 import run_zilany2009
+from .holmberg2007 import run_holmberg2007
+from .holmberg2007 import run_holmberg2007_vesicles
+from .zilany2014 import run_zilany2014
+from .zilany2014 import run_zilany2014_rate
+
+from .holmberg2007 import real_freq_map as freq_map_holmberg2007
+from .holmberg2007 import get_nearest_cf as get_nearest_cf_holmberg2007
 
 
-from cochlea.holmberg2007 import real_freq_map as freq_map_holmberg2007
-from cochlea.holmberg2007 import get_nearest_cf as get_nearest_cf_holmberg2007
-
-
-__version__ = "2"
-
-
-# Check if running 64-bit version of Python
-if sys.maxsize <= 2**32:
-    warnings.warn("cochlea: it seems that you are using 32-bit" +
-                  "version of Python." +
-                  "If you experience issues, please switch to 64-bit version.")
-
+__version__ = "1"
 
 
 def set_dbspl(signal, dbspl):
@@ -74,50 +43,6 @@ def set_dbspl(signal, dbspl):
     return scalled
 
 
-def set_dba_isolet(signal, dba):
-    p0 = 20e-6
-
-    # value from miclib (precalculated for all ISOLET files)
-    rms_dba = 0.02972401089
-
-    scaled = signal * 10**(dba/20) * p0 / rms_dba
-
-    return scaled
-
-
-def make_brian_group(trains):
-    """Create Brian's spike generator group from spike trains.
-
-    Parameters
-    ----------
-    trains : spike_trains
-        Input spike trains
-
-    Returns
-    -------
-    brian.SpikeGeneratorGroup
-        Brian's spike generator group.
-
-    """
-    import brian
-
-    times = []
-    indices = []
-    for i, spikes in enumerate(trains['spikes']):
-        times.append(spikes)
-        indices.append(np.ones(len(spikes)) * i)
-
-    indices = np.concatenate(indices)
-    times = np.concatenate(times) * brian.second
-
-    group = brian.SpikeGeneratorGroup(
-        len(trains),
-        spiketimes=(indices, times)
-    )
-
-    return group
-
-
 _greenwood_pars  = {
     'human': {'A': 165.4, 'a': 60, 'k': 0.88, 'length': 35e-3},
     'cat': {'A': 456, 'a': 84, 'k': 0.8, 'length': 25e-3},
@@ -131,7 +56,7 @@ def greenwood(x, species=None, A=None, a=None, k=None):
     basilar membrane.
     Center frequency and place on the basilar membrane can be
     connected by using the Greenwood equation [1].
-    .. math:: cf = A(10^{x \cdot a) - k)
+    .. math:: cf = A(10^{x \\cdot a) - k)
     where cf is the center frequency in Hz and x the place on the
     basilar membrane given from apex to base (apex = 0). The parameter
     A has the unit Hz, a has a unit of 1/m and k is unit less.
